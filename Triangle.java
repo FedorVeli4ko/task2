cd9638a46e4afffa4f6b3615cdb4eb2a428e9b5c
FedorVeli4ko <151904225+FedorVeli4ko@users.noreply.github.com>
2024-12-16 15:40:44 +0300

    Triangle.java:created

---
+++ Triangle.java
@@ -0,0 +1,24 @@
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0 ||
+                a + b <= c || a + c <= b || b + c <= a) {
+            throw new RuntimeException("There is no figure with such parameters.");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public double perimeter() {
+        return a + b + c;
+    }
+}
